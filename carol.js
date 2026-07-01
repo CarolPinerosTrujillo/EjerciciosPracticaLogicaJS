807751fa614155672844568991fc8c232a270ab4
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-07-01 10:49:17 -0500

    Primer ejercicio FizzBuzz

---
+++ carol.js
@@ -0,0 +1,33 @@
+let num=1
+
+if (num > 100) {
+    console.log("FIN")
+        
+} else {
+
+    while (num<=100) {
+        if (num % 3 ==0 && num %5==0) {
+            console.log("FizzBuzz");          
+        }else if (num % 5==0) {
+            console.log("Buzz");
+        }else if (num % 3 ==0) {
+            console.log("Fizz");
+        }else{
+            console.log(num);
+            
+        }
+        num++
+        }
+            
+            
+    }
+
+            
+console.log("HOLA TARADOS");
+
+        
+
+//for (let i = 1; i <= 100; i++) {
+  //  const element = array[index];
+    
+//}
